9a7d19a8202942168cb185a24317df6944ed716c
Sakthivel Nadar <55133980+SparXFusion@users.noreply.github.com>
2020-09-12 12:43:07 +0530

    RMX1831: build system and vendor libhwbinder

--- device.mk
+++ device.mk
@@ -110,6 +110,11 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service \
 
+# Binder
+PRODUCT_PACKAGES += \
+    libhwbinder \
+    libhwbinder.vendor
+
 #WiFi
 PRODUCT_PACKAGES += \
     hostapd \
