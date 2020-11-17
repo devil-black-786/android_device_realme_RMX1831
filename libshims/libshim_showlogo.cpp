7258961e6360b0a72a3aa1588d99c38dcabd3950
SamarV-121 <samarv121@pixelexperience.org>
2020-11-17 18:16:49 +0530

    RMX1831: update charger shim for R

--- libshims/libshim_showlogo.cpp
+++ libshims/libshim_showlogo.cpp
@@ -1,11 +1,12 @@
 #include <gui/SurfaceComposerClient.h>
 
 namespace android {
-    extern "C" void _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijPNS_14SurfaceControlENS_13LayerMetadataE(const String8& name, uint32_t w, uint32_t h, PixelFormat format, uint32_t flags, SurfaceControl* parent, android::LayerMetadata metadata);
+
+    extern "C" void _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijPNS_14SurfaceControlENS_13LayerMetadataEPj(const String8& name, uint32_t w, uint32_t h, PixelFormat format, uint32_t flags, SurfaceControl* parent, android::LayerMetadata metadata, uint32_t* outTransformHint);
 
     extern "C" void _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijPNS_14SurfaceControlEii(const String8& name, uint32_t w, uint32_t h, PixelFormat format, uint32_t flags, SurfaceControl* parent, int32_t windowType, int32_t ownerUid) {
         android::LayerMetadata metadata;
-        _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijPNS_14SurfaceControlENS_13LayerMetadataE(name, w, h, format, flags, nullptr, metadata);
+        _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijPNS_14SurfaceControlENS_13LayerMetadataEPj(name, w, h, format, flags, nullptr, metadata, nullptr);
     }
 
     extern "C" void _ZN7android21SurfaceComposerClient23getInternalDisplayTokenEv(void);
@@ -14,12 +15,14 @@ namespace android {
         _ZN7android21SurfaceComposerClient23getInternalDisplayTokenEv();
     }
 
-    extern "C" void _ZN7android14SurfaceControl7destroyEv(void);
+    extern "C" void _ZN7android14SurfaceControlD0Ev(void);
 
     extern "C" void _ZN7android14SurfaceControl5clearEv(void){
-        _ZN7android14SurfaceControl7destroyEv();
+        _ZN7android14SurfaceControlD0Ev();
     }
-
+    
+    extern "C" void _ZTVN7android13LayerMetadataE() { }
+    
     extern "C" void _ZN7android4base10StartsWithERKNSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEEPKc() { }
 
 }
